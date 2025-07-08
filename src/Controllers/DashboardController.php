e1183f5f473015d079599f6a59023f6029c2ffeb
juliocesar101290 <juliocesarleyvarodriguez@hotmail.com>
2025-07-08 10:17:54 -0700

    changes in dashboard generic

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -5,12 +5,18 @@ namespace julio101290\boilerplate\Controllers;
 /**
  * Class DashboardController.
  */
-class DashboardController extends BaseController
-{
-    public function index()
-    {
+class DashboardController extends BaseController {
+
+    public function index() {
+
+        helper('auth');
+
+        $idUser = user()->id;
+        $user = user()->username;
+
         $data = [
             'title' => 'Dashboard',
+            'userName' => $user,
         ];
 
         return view('julio101290\boilerplate\Views\dashboard', $data);
